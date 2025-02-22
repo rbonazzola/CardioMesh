7b7679e430a32824ef8ba337e7a8e3f16d1320a3
rbonazzola <rodbonazzola@gmail.com>
2025-02-22 17:43:28 -0300

    Added __init__ module

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 # For relative imports to work
 
 import os, sys; 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+sys.path.append(BASE_DIR := os.path.dirname(os.path.realpath(__file__)))
