769f10bffd56fdbe24c9a9055c7cc499fd6795c2
rbonazzola <rodbonazzola@gmail.com>
2025-02-23 12:37:00 -0300

    Added possibility to import Cardiac3DMesh without prefixing the module

--- cardio_mesh/__init__.py
+++ cardio_mesh/__init__.py
@@ -4,5 +4,7 @@ import os, sys;
 
 sys.path.append(BASE_DIR := os.path.dirname(os.path.realpath(__file__)))
 
-import CardiacMesh
+# import CardiacMesh
+from CardiacMesh import Cardiac3DMesh
+
 import procrustes
